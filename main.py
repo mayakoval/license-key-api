ba77e34c169b965e76aa117a2a7b5b641e3fa537
Maya Kowalewska <maja.kowalewska@yahoo.co.uk>
2023-03-12 23:37:25 +0000

    feat: key generator

--- main.py
+++ main.py
@@ -11,10 +11,20 @@ class RequestModel(BaseModel):
     software_package: str
 
 
+# key generator
+def key_generator(request: RequestModel):
+    api_key = "test"
+    full_key = request.full_name + request.software_package + api_key
+    return full_key
+
+
 # Endpoint generating key: takes name, software and secret
 @app.post("/key-generator/", dependencies=[Depends(auth.get_api_key)])
 async def generate_key(request: RequestModel):
-    return request
+    # generate key
+    generated_key = key_generator(request)
+    # hash the generated key
+    return generated_key
 
 
 # Endpoint validating the key
