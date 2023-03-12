8c298c2c916483e0eaf27b54c107dbff9488a737
Maya Kowalewska <maja.kowalewska@yahoo.co.uk>
2023-03-12 23:02:05 +0000

    feat: POST endpoint

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 # Model for request body
+class RequestModel(BaseModel):
+    full_name: str
+    software_package: str
+    secret: str
 
 # Endpoint generating key: takes name, software and secret
+@app.post("/key-generator/")
+async def generate_key(request):
+    return request
 
 # Endpoint validating the key
