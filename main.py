7501db30f918551461460b6846ffd3d8dbfe2c51
Maya Kowalewska <maja.kowalewska@yahoo.co.uk>
2023-03-13 09:23:47 +0000

    refactor: remove initial pseudocode

--- main.py
+++ main.py
@@ -10,25 +10,21 @@ app = FastAPI()
 
 load_dotenv(dotenv_path=".env")
 
-# Model for request body
+
 class RequestModel(BaseModel):
     full_name: str
     software_package: str
 
 
-# key generator
 def key_generator(request: RequestModel):
     api_key = os.getenv("API_KEY")
     full_key = request.full_name + request.software_package + api_key
     return full_key
 
 
-# Endpoint generating key: takes name, software and secret
 @app.post("/key-generator/", dependencies=[Depends(auth.get_api_key)])
 async def generate_key(request: RequestModel):
-    # generate key
     generated_key = key_generator(request)
-    # hash the generated key
     encrypted_key = await crypto.encrypt_key(generated_key)
     return {"key": encrypted_key}
 
